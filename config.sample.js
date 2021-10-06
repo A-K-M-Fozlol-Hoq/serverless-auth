8ac4db510d1457e0ec58d2d595255854403691fb
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2021-10-06 16:59:11 +0600

    connceted with database with error

---
+++ config.sample.js
@@ -0,0 +1,4 @@
+const MONGO_CONNECTION_STRING ='';
+module.exports = {
+	MONGO_CONNECTION_STRING
+}
--- handler.js
+++ handler.js
@@ -1,12 +1,23 @@
-'use strict';
+"use strict";
+const mongoose = require("mongoose");
+const {MONGO_CONNECTION_STRING}= require('./config')
+
 
 module.exports.hello = async (event) => {
+  const mongoose = require("mongoose");
+  mongoose.connect(MONGO_CONNECTION_STRING, {  useNewUrlParser: true, useUnifiedTopology: true,});
+// model = table structure
+  const Cat = mongoose.model("Cat", { name: String });
+//  create a new object
+  const kitty = new Cat({ name: "kitty-two" });
+ await  kitty.save();
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v2.0! Your function executed successfully!',
-        input: event,
+        message: "Go Serverless v2.0! Your function executed successfully!",
+        kitty
       },
       null,
       2
